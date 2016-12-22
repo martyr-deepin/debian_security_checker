e3ed79f02b71965837e2ea6fd8ba6f2f9cd14e7c
amazingfate <liujianfeng1994@gmail.com>
2016-12-22 14:43:01 +0800

    add date info

--- security_checker.py
+++ security_checker.py
@@ -19,12 +19,14 @@ test_list = ['https://lists.debian.org/debian-security-announce/2016/msg00325.ht
 def get_package_info(url):
     package_info = {}
     url_text = requests.get(url).text
+    re_date = re.compile(".+Date.+\:.+\, (.+)\<\/li\>")
     re_package = re.compile("Package        : (.+)")
     re_cve = re.compile("CVE ID         : (.+)")
     re_debian_bug = re.compile("Debian Bug     : (.+)")
     re_stable = re.compile("For the stable.+fixed in\\nversion (.+)\.")
     re_unstable = re.compile("For the unstable.+fixed in\\nversion (.+)\.")
     package_name = re_package.findall(url_text)[0]
+    _date = re_date.findall(url_text)[0]
     if len(re_cve.findall(url_text)):
         cve_id = re_cve.findall(url_text)[0]
     else:
@@ -42,6 +44,7 @@ def get_package_info(url):
         unstable_version = re_unstable.findall(url_text)[0]
     else:
         unstable_version = 'none'
+    package_info["date"] = _date
     package_info["package"] = package_name
     package_info["CVE_ID"] = cve_id
     package_info["debian_bug"] = debian_bug
